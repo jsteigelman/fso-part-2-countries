61ef7c3d0d17c1b588a6c58b2cac67265fb5082d
Joey Steigelman <joey.steigelman@gmail.com>
2022-03-30 13:54:19 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
 
-function App() {
+const axios = require('axios');
+const countriesApi = 'https://restcountries.com/v3.1/all' 
+
+const App = () => {
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+    axios
+      .get(countriesApi)
+      .then((response) => {
+        const countries = response.data
+        console.log(countries)
+        setCountries(countries)
+      })
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>hi</p>
     </div>
   );
 }
