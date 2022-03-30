1f9c9ef5f769107f807d1c91555cc59789dae23c
Joey Steigelman <joey.steigelman@gmail.com>
2022-03-30 15:53:28 -0400

    display appropriate message depending on number of results returned

--- src/Components/CountryList.js
+++ src/Components/CountryList.js
@@ -1,11 +1,24 @@
 import Country from './Country'
 
 const CountryList = ({ filteredCountries }) => {
-    return (
-        filteredCountries.map((country) => (
-            <Country country={country} />
-          )
-    ))
+
+  const filteredResults = filteredCountries.map((country) => (
+    <Country country={country} />
+  ))
+
+  const results = () => {
+    if (filteredCountries.length === 0) {
+      return <p>Get started by entering a search term</p>
+    } else if (filteredCountries.length > 10) {
+      return <p>Your search returned {filteredCountries.length} results. Please narrow your search and try again.</p>
+    } else {
+      return filteredResults
+    }
+  }
+
+  return (
+      <div>{results()}</div>
+)
 }
 
 export default CountryList
