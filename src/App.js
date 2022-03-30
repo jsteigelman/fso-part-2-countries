6e0f4b8d08ced7e3b62e353391276c6293ded6d6
Joey Steigelman <joey.steigelman@gmail.com>
2022-03-30 15:17:49 -0400

    user can search for countries by keyword and see list of matching results

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import { useState, useEffect } from 'react'
 
-const axios = require('axios');
-const countriesApi = 'https://restcountries.com/v3.1/all' 
+const axios = require('axios')
+
+const Country = ({country}) => {
+  return (
+    <li key={country.name.common}>{country.name.common}</li>
+  )
+}
 
 const App = () => {
   const [countries, setCountries] = useState([])
+  const [searchTerm, setSearchTerm] = useState('')
+  const [filteredCountries, setFilteredCountries] = useState([])
+
+  const countriesApi = 'https://restcountries.com/v3.1/all'
 
   useEffect(() => {
-    axios
-      .get(countriesApi)
-      .then((response) => {
-        const countries = response.data
-        console.log(countries)
-        setCountries(countries)
-      })
-  })
+    axios.get(countriesApi).then((response) => {
+      const countries = response.data
+      setCountries(countries)
+    })
+  }, [])
+
+  const handleFilter = (event) => {
+    const userFilter = event.target.value
+    setSearchTerm(userFilter)
+  }
+
+  const applyFilter = (event) => {
+    event.preventDefault()
+    const myFilter = countries.filter((countryObject) => {
+      return countryObject.name.common.includes(searchTerm)
+    })
+    setFilteredCountries(myFilter)
+    setSearchTerm('')
+  }
+
+  const displayCountries = filteredCountries.map((country) => <Country country={country} />)
 
   return (
     <div>
-      <p>hi</p>
+      <p>Search Countries</p>
+      <form onSubmit={applyFilter}>
+        <input type='text' value={searchTerm} onChange={handleFilter} />
+      </form>
+      <div>
+      {displayCountries}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
